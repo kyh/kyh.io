793f9e31c362e3fe4be490013f1f184677fba7c7
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-10-16 23:31:11 -0700

    Added wct conf for integration testing

---
+++ wct.conf.js
@@ -0,0 +1,15 @@
+module.exports = {
+  verbose: true,
+  testTimeout: 5 * 60 * 1000,
+  plugins: {
+    sauce: {
+      disabled: true,
+      browsers: [
+        "Windows 8.1/internet explorer",
+        "OS X 10.10/chrome",
+        "OS X 10.10/firefox",
+        "OS X 10.10/safari"
+      ]
+    }
+  }
+};
