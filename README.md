9f29beba819dd293d0439ee52486a5410c8f2c0e
Kaiyu Hsu <im.kaiyu@gmail.com>
2017-02-19 21:34:13 -0800

    Add revealjs, styles and update README

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ kyh.io
 [![Build Status](https://travis-ci.org/tehkaiyu/tehkaiyu.github.io.svg?branch=master)](https://travis-ci.org/tehkaiyu/tehkaiyu.github.io)
 [![NPM version](https://badge.fury.io/js/badge-list.svg)](http://badge.fury.io/js/badge-list)
 
-Powered by Polymer.
+Made with lots of love, and some HTML.
--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
+import './style.css';
+import RevealFx from './reveal';
+import anime from 'animejs';
+
+function revealElement(elementClass, bgcolor, delay) {
+  const $el = new RevealFx(document.querySelector(elementClass), {
+    revealSettings: {
+      bgcolor,
+      delay,
+      easing: 'easeOutExpo',
+      direction: 'lr',
+      onStart: function(contentEl) {
+        anime.remove(contentEl);
+        contentEl.style.opacity = 0;
+      },
+      onCover: function(contentEl) {
+        anime({
+          targets: contentEl,
+          duration: 800,
+          delay: 80,
+          easing: 'easeOutExpo',
+          translateX: [-40, 0],
+          opacity: [0, 1]
+        });
+      }
+    }
+  });
+  $el.reveal();
+}
+
+revealElement('.title', '#7f40f1', 300);
+revealElement('.subtitle', '#fcf652', 550);
+
 console.log('hello world');
