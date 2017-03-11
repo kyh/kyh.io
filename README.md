59ec15243bf4e9d898716a6e30e5e8470e76bc73
Kaiyu Hsu <im.kaiyu@gmail.com>
2017-03-10 17:25:25 -0800

    Migrated to use Vue

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
-kyh.io
-==================
+# kyh.io
+
 [![Build Status](https://travis-ci.org/tehkaiyu/tehkaiyu.github.io.svg?branch=master)](https://travis-ci.org/tehkaiyu/tehkaiyu.github.io)
 [![NPM version](https://badge.fury.io/js/badge-list.svg)](http://badge.fury.io/js/badge-list)
 
-Made with lots of love, and some HTML.
+Made with lots of love, and some JavaScript.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run all tests
+npm test
+```
+
---
+++ src/components/Button.vue
@@ -0,0 +1,65 @@
+<template>
+  <a class="link" href="#" :data-letters="title">{{title}}</a>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: ['title'],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.link {
+  font-weight: 500;
+  outline: none;
+  text-decoration: none;
+  position: relative;
+  line-height: 1;
+  display: inline-flex;
+  overflow: hidden;
+  line-height: 1;
+  border-bottom: 1px dotted #6b74e6;
+  color: #6b74e6;
+  transition: border-color 0.4s ease;
+}
+
+.link:hover {
+  color: #6b74e6;
+  border-color: #424242;
+}
+
+.link::after {
+  content: '';
+  position: absolute;
+  height: 3px;
+  width: 100%;
+  top: 50%;
+  margin-top: -1px;
+  right: 0;
+  background: #F9F9F9;
+  transform: translate3d(-100%,0,0);
+  transition: transform 0.4s;
+  transition-timing-function: cubic-bezier(0.7,0,0.3,1);
+}
+
+.link:hover::after {
+  transform: translate3d(100%,0,0);
+}
+
+.link::before {
+  content: attr(data-letters);
+  position: absolute;
+  z-index: 2;
+  overflow: hidden;
+  color: #424242;
+  white-space: nowrap;
+  width: 0%;
+  transition: width 0.4s 0.3s;
+}
+
+.link:hover::before {
+  width: 100%;
+}
+</style>
