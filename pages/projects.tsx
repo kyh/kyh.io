dd2ef7b750b27143e6b0430fb7e018a7bb9636d0
Kaiyu Hsu <im.kaiyu@gmail.com>
2022-02-10 00:07:16 -0800

    add links to projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import { SEO } from "@components/SEO";
 import { AnimateText } from "@components/AnimateText";
+import { TippyLink } from "@components/TippyLink";
 import styles from "styles/Page.module.css";
 
 const Page: NextPage = () => {
@@ -19,19 +20,42 @@ const Page: NextPage = () => {
       <section className={styles.grid}>
         <div>
           <h2 className={styles.caption}>Side Projects</h2>
-          <ul className={`${styles.list} ${styles.listLarge}`}>
-            <li>Yours Sincerely</li>
-            <li>Playhouse</li>
-            <li>UI Capsule</li>
-            <li>Inteligir</li>
+          <ul className={`${styles.list} ${styles.projectList}`}>
+            <li>
+              <TippyLink
+                text="Yours Sincerely"
+                href="https://yourssincerely.org"
+              />
+            </li>
+            <li>
+              <TippyLink text="Playhouse" href="https://playhouse.gg" />
+            </li>
+            <li>
+              <TippyLink text="UI Capsule" href="https://uicapsule.com" />
+            </li>
+            <li>
+              <TippyLink text="Inteligir" href="https://inteligir.com" />
+            </li>
           </ul>
         </div>
         <div>
           <h2 className={styles.caption}>Consulting</h2>
-          <ul className={`${styles.list} ${styles.listLarge}`}>
-            <li>Founding</li>
-            <li>Mederva Health</li>
-            <li>Local Kitchens</li>
+          <ul className={`${styles.list} ${styles.projectList}`}>
+            <li>
+              <TippyLink text="Founding" href="https://founding.so" />
+            </li>
+            <li>
+              <TippyLink
+                text="Mederva Health"
+                href="https://medervahealth.com"
+              />
+            </li>
+            <li>
+              <TippyLink
+                text="Local Kitchens"
+                href="https://localkitchens.com/"
+              />
+            </li>
           </ul>
         </div>
       </section>
