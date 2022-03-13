8e20152d03e7c7c1294ffbad7db481ce98ff996d
Kaiyu Hsu <im.kaiyu@gmail.com>
2022-03-13 06:11:41 -0700

    remove tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 import Router from "next/router";
 import nprogress from "nprogress";
-import Script from "next/script";
 import { ThemeProvider } from "next-themes";
 import { Dock } from "@components/Dock";
 import "styles/globals.css";
@@ -13,11 +12,6 @@ Router.events.on("routeChangeComplete", nprogress.done);
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
-      <Script
-        strategy="afterInteractive"
-        src="https://static.cloudflareinsights.com/beacon.min.js"
-        data-cf-beacon='{"token": "0e42005637254ecab41ca2bdd010d5b9"}'
-      />
       <Component {...pageProps} />
       <Dock />
     </ThemeProvider>
