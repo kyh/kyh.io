8323106021bb046b3e15b8f0d39b8a3f912a8731
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-04-26 02:36:17 -0700

    remove some content security issues

--- next.config.js
+++ next.config.js
@@ -37,8 +37,6 @@ const nextConfig = {
 
 // https://nextjs.org/docs/advanced-features/security-headers
 const ContentSecurityPolicy = `
-    default-src 'self' vercel.live;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live;
     style-src 'self' 'unsafe-inline';
     img-src * blob: data:;
     media-src 'self';
