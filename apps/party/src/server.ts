24fe343a001cbb69a85ee4a41f6fdc275f5398c5
Kaiyu Hsu <im.kaiyu@gmail.com>
2025-09-12 10:15:05 -0700

    fix some changes

--- apps/party/src/server.ts
+++ apps/party/src/server.ts
@@ -1,15 +1,49 @@
 import type { Connection } from "partyserver";
 import { routePartykitRequest, Server } from "partyserver";
 
-import type {
-  Player,
-  PlayerMap,
-  PositionMessage,
-  RemoveMessage,
-  SyncMessage,
-  UpdateMessage,
-} from "../src/lib/player";
-import { getColorById } from "../src/lib/color";
+import { getColorById } from "./color";
+
+// Schema
+export type Position = {
+  x?: number;
+  y?: number;
+  pointer?: "mouse" | "touch";
+  pathname?: string;
+};
+
+export type Player = {
+  id: string;
+  color?: string;
+  hue?: string;
+  lastUpdate?: number;
+  position?: Position;
+};
+
+export type PlayerMap = Record<string, Player>;
+
+// These are messages coming from the client
+export type PositionMessage = {
+  type: "position";
+  data: Position;
+};
+
+// These are messages coming from the server
+export type UpdateMessage = {
+  type: "update";
+  data: Player;
+};
+
+export type SyncMessage = {
+  type: "sync";
+  data: PlayerMap;
+};
+
+export type RemoveMessage = {
+  type: "remove";
+  data: { id: string };
+};
+
+export type MessageType = UpdateMessage | SyncMessage | RemoveMessage;
 
 type Env = {
   KyhServer: DurableObjectNamespace<KyhServer>;
