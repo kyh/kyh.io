93edb1b3f3a98b9616aacd5bfa2324ddaf8a47be
Kaiyu Hsu <im.kaiyu@gmail.com>
2025-11-03 08:49:22 -0800

    remove rivet

--- apps/kyh/src/app/api/rivet/[...all]/route.ts
+++
@@ -1,7 +0,0 @@
-import { toNextHandler } from "@rivetkit/next-js";
-
-import { registry } from "@/lib/rivet-registry";
-
-export const maxDuration = 300;
-
-export const { GET, POST, PUT, PATCH, HEAD, OPTIONS } = toNextHandler(registry);
